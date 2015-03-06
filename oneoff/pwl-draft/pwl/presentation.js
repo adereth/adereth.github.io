5655ef614712e7c6d257f04b963f01b1af8a1c9f
Matt Adereth <adereth@gmail.com>
2015-03-06 08:02:47 -0800

    Site updated at 2015-03-06 16:02:47 UTC

---
+++ oneoff/pwl-draft/pwl/presentation.js
@@ -0,0 +1,38 @@
+delay = 250;
+currentPage = 0;
+keyHandlerForPage = {};
+
+document.addEventListener('DOMContentLoaded', function() {
+    slides = $(".slide");
+    slides.eq(currentPage).fadeIn(1000, "swing");
+    var usedPageHandler = false;
+    nextPageHandler = null;
+    turnThePage = function(e) {
+        var handler = keyHandlerForPage[slides.eq(currentPage).attr("id")];
+        if (nextPageHandler) {
+            var toCall = nextPageHandler;
+            nextPageHandler = null;
+            toCall();
+        } else if (!usedPageHandler && handler) {
+            usedPageHandler = true;
+            handler();
+        } else {
+            nextPageHandler = null;
+            usedPageHandler = false;
+            var key = e.keyCode ? e.keyCode : e.which;
+            var oldPage = currentPage;
+            if (key == 39) {
+                currentPage++;
+            } else if (key == 37 && currentPage > 0) {
+                currentPage--;
+            }
+            if (currentPage != oldPage) {
+                slides.eq(oldPage).fadeOut(delay, "swing", function() {
+                    slides.eq(currentPage).fadeIn(delay, "swing");
+                });
+            }
+        }
+    };
+
+    window.onkeyup = turnThePage;
+});
